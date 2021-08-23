c5e2d9ad61d9dac4f86a84cd831a13c8cb6d7c7c
Shibaji Chakraborty <shibaji7@vt.edu>
2021-08-23 15:49:36 +0000

    FITS Level data analysis

---
+++ v2.0/chips_fits.py
@@ -0,0 +1,66 @@
+"""chips_fits.py: Module is used to implement edge detection tecqniues using thresholding"""
+
+__author__ = "Chakraborty, S."
+__copyright__ = ""
+__credits__ = []
+__license__ = "MIT"
+__version__ = "1.0."
+__maintainer__ = "Chakraborty, S."
+__email__ = "shibaji7@vt.edu"
+__status__ = "Research"
+
+import os
+
+import astropy.units as u
+from sunpy.net import Fido, attrs
+import sunpy.map
+from aiapy.calibrate import register, update_pointing, normalize_exposure
+
+class RegisterAIA(object):
+    
+    def __init__(self, date, wavelength=193, resolution=4096, vmin=10):
+        self.wavelength = wavelength
+        self.date = date
+        self.resolution = resolution
+        self.vmin = vmin
+        self.normalized()
+        return
+    
+    def normalized(self):
+        q = Fido.search(
+            attrs.Time(self.date.strftime("%Y-%m-%dT%H:%M:%S"), (self.date + dt.timedelta(seconds=11)).strftime("%Y-%m-%dT%H:%M:%S")),
+            attrs.Instrument("AIA"),
+            attrs.Wavelength(wavemin=self.wavelength*u.angstrom, wavemax=self.wavelength*u.angstrom),
+        )
+        self.m = sunpy.map.Map(Fido.fetch(q[0,0]))
+        m_updated_pointing = update_pointing(self.m)
+        m_registered = register(m_updated_pointing)
+        self.m_normalized = normalize_exposure(m_registered)
+        return
+    
+class CHIPS(object):
+    
+    def __init__(self, _dict_, local="tmp/{date}/"):
+        self._dict_ = _dict_
+        self.aia = RegisterAIA(_dict_["date"], _dict_["wavelength"], _dict_["resolution"], _dict_["vmin"])
+        self.folder = local.format(date=_dict_["date"].strftime("%Y-%m-%d-%H-%M"))
+        if not os.path.exists(self.folder): os.system("mkdir -p " + self.folder)
+        self.saveimg()
+        return
+    
+    def saveimg(self):
+        file = self.folder + "01_raw.png"
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        self.aia.m_normalized.plot(annotate=False, axes=ax, vmin=self._dict_["vmin"])
+        self.aia.m_normalized.draw_limb()
+        fig.savefig(file, bbox_inches="tight")
+        return
+    
+if __name__ == "__main__":
+    _dict_ = {}
+    _dict_["date"] = dt.datetime(2018,5,30,12)
+    _dict_["wavelength"] = 193
+    _dict_["resolution"] = 1024
+    _dict_["vmin"] = 35
+    CHIPS(_dict_)
