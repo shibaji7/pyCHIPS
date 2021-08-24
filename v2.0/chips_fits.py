d1cf422be736f41f508e1898e334103af7d83ea1
Shibaji Chakraborty <shibaji7@vt.edu>
2021-08-24 14:37:25 +0000

    v2.0

--- v2.0/chips_fits.py
+++ v2.0/chips_fits.py
@@ -9,6 +9,10 @@ __maintainer__ = "Chakraborty, S."
 __email__ = "shibaji7@vt.edu"
 __status__ = "Research"
 
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
 import os
 import datetime as dt
 
